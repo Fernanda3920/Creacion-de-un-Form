5f6c7d45b652a9d02dbad7b9dc04568eb4ae34b2
Fernanda3920 <167030876+Fernanda3920@users.noreply.github.com>
2024-04-14 13:39:06 -0700

    Creación de un Form
    El siguiente programa usa la implementación de un form desde un proyecto de tipo consola

---
+++ FormBotoncito/FormBotoncito/Program.cs
@@ -0,0 +1,40 @@
+﻿using FormBotoncito;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormBotoncito
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            //INSTANCIAR LOS COMPONENTES
+            Form1 form1 = new Form1();
+            Button boton1 = new Button();
+
+            //PROPIEDADES DE LOS COMPONENTES
+            boton1.Text = "HOLA!";
+            form1.BackColor = Color.Pink;  
+            boton1.BackColor = Color.Pink;
+            boton1.ForeColor = Color.White;  
+            boton1.Width = 150;
+            boton1.Height = 75;
+            boton1.Font = new Font("Arial", 12, FontStyle.Bold);
+            boton1.Top = (form1.Height - boton1.Width) / 2;
+            boton1.Left = (form1.Width - boton1.Width) / 2;
+
+            
+            //MOSTRAR LOS COMPONENTES
+            form1.Show();
+            form1.Controls.Add(boton1);
+            Application.Run(form1); 
+        }
+    }
+}
